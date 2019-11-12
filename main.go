6a4f27a49bcdc941d9891f1bd5c108ba115e0b4e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-11-12 18:39:26 -0500

    Add handlers

--- main.go
+++ main.go
@@ -15,11 +15,17 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", helloWorld).Methods("GET")
+	router.HandleFunc("/users", AllUsers).Methods("GET")
+	router.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
+	router.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
+	router.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
 
 func main() {
 	fmt.Println("Go ORM Example")
+
+	InitialMigration()
 
 	handleRequests()
 }
