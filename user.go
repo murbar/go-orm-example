a2b21218195ac6187d3ff3dc424b651806ae417e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-11-13 12:55:46 -0500

    Move to Gin

--- user.go
+++ user.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
